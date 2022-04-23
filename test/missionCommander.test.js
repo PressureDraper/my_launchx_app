d1cf275b4ca1b26b4b52eefd800eba738d84ece4
PressureDraper <warmarines00@hotmail.com>
2022-04-23 17:20:26 -0500

    add: first test for TDD

---
+++ test/missionCommander.test.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./../app/missionCommander.js');
+
+describe("Esto es una suite de pruebas", () => {
+    test('Caso de prueba 1', () => {
+        const result = 1 + 2 
+        expect(result).toBe(3);
+    });
+})
