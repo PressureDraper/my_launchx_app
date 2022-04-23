5beefd7fc529e0abf2bb690bd6319bdd59fb67e0
PressureDraper <warmarines00@hotmail.com>
2022-04-23 17:29:53 -0500

    add: testig MC class

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,8 +1,9 @@
 const MissionCommander = require('./../app/missionCommander.js');
 
-describe("Esto es una suite de pruebas", () => {
-    test('Caso de prueba 1', () => {
-        const result = 1 + 2 
-        expect(result).toBe(3);
+describe("Unit Tests for Mission Commander Class", () => {
+    test('Create a mission commander object', () => {
+        const myMissionCommander = new MissionCommander("Woopa")
+
+        expect(myMissionCommander.name).toBe("Woopa");
     });
 })
